c2c62944180456142d6378aba6435b2c4cc2baf8
zhangxuefei <mszhangxuefei@qq.com>
2021-01-01 11:13:17 +0800

    update

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 // 微信 api promise 化
-import {
-  promisifyAll
-} from "miniprogram-api-promise";
+import { promisifyAll } from "miniprogram-api-promise";
 const wxp = {};
 promisifyAll(wx, wxp);
 // 覆盖原有的 wx 对象
 wx = wxp;
+
+// 重写 Page 实现全局默认分享
+
+!(function () {
+  const PageTemp = Page;
+  Page = function (pageConfig) {
+    pageConfig = {
+      onShareAppMessage: function () {
+        return {};
+      },
+      ...pageConfig,
+    };
+    PageTemp(pageConfig);
+  };
+})();
 
 App({
   onLaunch(options) {},
